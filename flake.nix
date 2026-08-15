bc982e146c9de31c4d694cc92bd54f8591b8b5c3
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-15 15:55:48 +0200

    chore: use GitHub project sources

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for the itempool crate";
 
   inputs = {
-    rs-harbor.url = "git+ssh://git@codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
+    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
     crane.url = "github:ipetkov/crane";
 
     flake-utils.url = "github:numtide/flake-utils";
